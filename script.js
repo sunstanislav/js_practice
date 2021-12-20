188a630ab2a942815ae7b762f955b0de03a11a27
sunstanislav <sundeev91@ya.ru>
2021-12-20 10:28:28 +0600

    1 task

--- script.js
+++ script.js
@@ -21,4 +21,22 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); //+ - указывает на число
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {}, // {} - обьект
+    actors: {},
+    genres: [], // [] - массив
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''), // , '' - значение по умолчанию
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+ console.log(personalMovieDB);
